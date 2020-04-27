6b6e3aac5eb2bb4db10ae7572740e4f50e382628
Syed AbuTalib <syedabutalib@github.com>
2020-04-27 01:03:46 -0500

    renamed temp_var to list

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ post('/') do
   username = params[:username] || 'alipervaiz'
   g = GithubWrapper.new
   list = g.goodies(username)
-  @temp_var = list
+  @list = list
   @repo = Octicons::Octicon.new('repo')
   @dot = Octicons::Octicon.new('primitive-dot')
   @star = Octicons::Octicon.new('star')
@@ -29,7 +29,7 @@ post('/') do
   @lc = LanguageColors::LanguageColors.new
   # puts Gem.loaded_specs.values.map(&:full_gem_path)
 
-  erb :masonry, locals: { list: @temp_var, 'username' => username,
+  erb :masonry, locals: { list: @list, 'username' => username,
                           repo: @repo.to_svg, dot: @dot.to_svg,
                           star: @star.to_svg, forked: @forked.to_svg, lc: @lc }
 end
